b3a29e3f6b4ae1402575e9f527861ea4899ae906
nrapp523 <nrapp523@gmail.com>
2026-04-27 20:17:46 -0600

    Prep 3 assignments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        //ask user for magic number and store it
+        /*string magicNum;
+        int magicNumInt;
+        Console.Write("What is the magic number? ");
+        magicNum = Console.ReadLine();
+        magicNumInt = int.Parse(magicNum);
+        */
+
+
+        //loop until user guesses right
+        int userGuessInt;
+        int magicNum;
+        //generate new random variable
+        Random randomGenerator = new Random();
+        magicNum = randomGenerator.Next(1, 100);
+        int i = 0;
+        do
+        {
+
+            i += 1;
+            //take users response
+            string userGuess;
+            Console.Write("What is your guess? ");
+            userGuess = Console.ReadLine();
+            userGuessInt = int.Parse(userGuess);
+            //conditional based on higher or lower
+            if (userGuessInt > magicNum)
+            {
+                Console.WriteLine("Go Lower!");
+            }
+            else if(userGuessInt < magicNum)
+            {
+               Console.WriteLine("Go higher!");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+
+        } while (userGuessInt != magicNum);
+        Console.WriteLine($"Completed in {i} attempts!")
+        
+    }   
 }
