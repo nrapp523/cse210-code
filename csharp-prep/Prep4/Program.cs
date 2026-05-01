97171f3191f85d93f342698813b289c0988ca499
nrapp523 <nrapp523@gmail.com>
2026-04-30 20:43:28 -0600

    prep 4 assignments

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, and type o when finished!");
+        //variable for to determine runtime of do while loop
+        bool isFinished = false;
+        string userInput;
+        int integerHolder;
+        List<int> numList = new List<int>();
+        
+        //loop for retrieving user input
+        do
+        {
+            
+    
+
+            Console.Write("Enter Number:");
+            userInput = Console.ReadLine();
+            integerHolder = int.Parse(userInput);
+            if (integerHolder == 0)
+            {
+                isFinished = true;
+            }
+            else 
+            {
+                numList.Add(integerHolder);
+            }
+            
+
+            
+        } while (isFinished == false);
+        
+        Console.WriteLine($"The sum is: {numList.Sum()}");
+        Console.WriteLine($"The average is: {numList.Average()}");
+        Console.WriteLine($"The largest number is: {numList.Max()}");
+
     }
 }
