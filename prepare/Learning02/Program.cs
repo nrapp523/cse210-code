c0e21b362608f4205adad4bca90c08d3bbfae193
nrapp523 <nrapp523@gmail.com>
2026-05-04 22:28:37 -0600

    Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._company = "apple";
+        job1._endYear = 2024;
+        job1._startYear = 2012;
+        job1._jobTitle = "Software Engineer";
+        job2._company = "Microsoft";
+        job2._endYear = 2010;
+        job2._startYear = 2004;
+        job2._jobTitle = "Cyber Analyst";
+
+     
+        Resume myResume = new Resume();
+        myResume._name = "Jacob";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        Console.WriteLine($"Name: {myResume._name}");
+        Console.WriteLine("Jobs:");
+        myResume.Display();
+        
+
+
+
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,14 @@
+public class Resume()
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        foreach (Job j in _jobs)
+        {
+            j.Display();
+        }
+    
+    }
+}
