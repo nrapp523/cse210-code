be65e0338165c87e5d26fbe6698543454ac73c99
nrapp523 <nrapp523@gmail.com>
2026-05-30 19:43:15 -0600

    Final Program fix

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,12 +1,60 @@
 using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Reference newReference = new Reference("1 Nephi", 11, 12);
-        Console.WriteLine(newReference.GetReference());
-        newReference.SetReference("2 Nephi", 2, 25, 27);
-        Console.WriteLine(newReference.GetReference());
+        Scriptures myScripture = new Scriptures("For behold, this is my work and my glory to bring to pass the immortality and eternal life of man.");
+        Reference myReference = new Reference("Moses", 1, 39);
+
+        bool repeatLoop = true;
+
+        void DisplayResults()
+        {
+            Console.Clear();
+            Console.WriteLine(myReference.GetReference());
+            Console.WriteLine(myScripture.GetVerse());
+            Console.Write("Press enter to continue or q to quit.");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+        }
+        
+        do
+        {
+            Console.Clear();
+            Console.WriteLine(myReference.GetReference());
+            Console.WriteLine(myScripture.GetVerse());
+            Console.Write("Press enter to continue or q to quit.");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+
+            if(keyInfo.Key == ConsoleKey.Enter)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (myScripture.GetIndexList().Count() > 0)
+                    {
+                        myScripture.CreateRandomIndex();
+                        myScripture.ReplaceWord();
+                        myScripture.UpdateVerse();
+                    }
+                    else
+                    {
+                        DisplayResults();
+                        repeatLoop = false;
+                        break;
+                    }
+                    
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.Q)
+            {
+                repeatLoop = false;
+            }
+
+
+        } while (repeatLoop);
+
     }
 }
