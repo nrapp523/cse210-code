524bc8dd9382fddd8a281451323a44a5b900a59c
nrapp523 <nrapp523@gmail.com>
2026-06-01 18:13:48 -0600

    Assignment class

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,16 @@
+class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetSummary()
+    {
+        return $"Name: {_studentName}, topic: {_topic}";
+    }
+}
